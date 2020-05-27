4497878ef77b4ddc43230fa240c319a580e83ee7
lovanto <rifkylovanto@gmail.com>
2020-05-27 18:17:53 +0700

    daily alarm setup, need to fix when notif clicked

--- app/src/main/java/com/lovanto/sosdev/view/SettingActivity.kt
+++ app/src/main/java/com/lovanto/sosdev/view/SettingActivity.kt
@@ -1,13 +1,62 @@
 package com.lovanto.sosdev.view
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.lovanto.sosdev.R
+import com.lovanto.sosdev.alarm.AlarmReceiver
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
+
+    companion object {
+        const val PREFS_NAME = "SettingPref"
+        private const val DAILY = "daily"
+    }
+
+    private lateinit var alarmReceiver: AlarmReceiver
+    private lateinit var mSharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title = "Setting"
+
+        alarmReceiver = AlarmReceiver()
+        mSharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+        setSwitch()
+
+        swDaily.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                alarmReceiver.setDailyReminder(
+                    this,
+                    AlarmReceiver.TYPE_DAILY,
+                    getString(R.string.daily_message)
+                )
+            } else {
+                alarmReceiver.cancelAlarm(this, AlarmReceiver.TYPE_DAILY)
+            }
+            saveChange(DAILY, isChecked)
+        }
+    }
+
+    private fun setSwitch() {
+        swDaily.isChecked = mSharedPreferences.getBoolean(DAILY, false)
+    }
+
+    private fun saveChange(key: String, value: Boolean) {
+        val editor = mSharedPreferences.edit()
+        editor.putBoolean(key, value)
+        editor.apply()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) finish()
+
+        return super.onOptionsItemSelected(item)
     }
 }
